15e4037ccda52b4643c647815a2c3adf5c7cb0e5
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-03-16 09:22:08 -0300

    Adicionar arquivos de projeto.

---
+++ ContaOnline.Domain/Interfaces/IContaCategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using ContaOnline.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaOnline.Domain.Interfaces
+{
+    public interface IContaCategoriaRepository
+    {
+        void Incluir(ContaCategoria contaCategoria);
+        void Alterar(ContaCategoria contaCategoria);
+        void Excluir(string id);
+        ContaCategoria ObterPorId(string id);
+        IEnumerable<ContaCategoria> ObterTodos();
+    }
+}
---
+++ ContaOnline.Domain/Models/ContaCategoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaOnline.Domain.Models
+{
+    public class ContaCategoria
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
